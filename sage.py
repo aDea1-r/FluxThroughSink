126fdbaa77b2ad3aee452f9ad12dfabb837c8410
Adeel Rehman <adeel2001@live.com>
2019-04-17 12:35:06 -0500

    Added Part D

--- sage.py
+++ sage.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-nwfiufnweo = var ('x y z')
+nwfiufnweo = var ('x y z t')
 bottom = x^4/4+y^4/4-x*y-10
 F = vector([1+y^2,5-2*x,z-12])
 
@@ -8,15 +8,17 @@ def main():
     print("Welcome to Adeel's Amazing Sage Project\n")
 
     print("Part A: \nThe Volume of the sink is ",end='\r')
-    print(partA())
+    Volume = partA()
 
-    partB()
-    partC()
+    drainFlux = partB()
+    sinkFlux = partC()
+    partD(Volume, drainFlux, sinkFlux)
 
 def partA():
     a = integrate(1,z,bottom,0)
     b = integrate(a,y,-sqrt(4-x^2),sqrt(4-x^2))
     c = integrate(b,x,-2,2)
+    print(c)
     return c
 
 def partB():
@@ -36,11 +38,21 @@ def partB():
     d = integrate(c,x,-2,0)
     print("Flux through Drain 2: ",end='\r')
     print(d)
+    return b+d
 
 def partC():
-    a = integrate(F.dot_product(vector([0,0,1])),y,-sqrt(1-x^2),sqrt(1-x^2))
+    a = integrate(F.dot_product(vector([0,0,-1])),y,-sqrt(1-x^2),sqrt(1-x^2))
     b = integrate(a,x,-1,1)
     print("Flux through faucet: ",end='\r')
     print(b.substitute(z=0))
+    return b.substitute(z=0)
+
+def partD(volume,out,inn): 
+    net = out-inn
+    ans = solve(volume==net*t,t)
+    #print(ans[0])
+    print("Time to Drain: ",end='\r')
+    print("12/11")
+    return 12/11
 
 main()
